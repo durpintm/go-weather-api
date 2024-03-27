8e52650217ea4a33ac1455bf10e87a65dbbceaf4
Durpin Thapa Magar <durpinthapa@gmail.com>
2024-03-27 01:04:57 -0400

    Create function loadApiConfig to access api key

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/durpintm/go-weather-api
 
 go 1.21.6
+
+require github.com/gorilla/mux v1.8.1
--- main.go
+++ main.go
@@ -1,5 +1,65 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type apiConfigData struct{
+	OpenWeatherMapApiKey string `json:"OpenWeatherMapApiKey"`
+}
+
+type WeatherRequest struct{
+	Name string `json:"name"`
+}
+
+type WeatherResponse struct {
+    Name string `json:"name"`
+    Main struct {
+        Temp float64 `json:"temp"`
+    } `json:"main"`
+    Weather []struct {
+        Description string `json:"description"`
+    } `json:"weather"`
+}
+
+const Dport = ":8000"
+
 func main(){
-	
+
+    // router := mux.NewRouter()
+
+    // router.HandleFunc("/weather/city", getWeatherByCityHandler).Methods("GET")
+    // router.HandleFunc("/weather/city", weatherHandler).Methods("POST")
+
+	// fmt.Printf("Server is starting on port: %v\n", Dport)
+
+	// http.ListenAndServe(Dport, router)
+
+    apiKey,err := loadApiConfig(".apiConfig")
+    if(err != nil){
+        fmt.Println("Error accessing api key", err)
+    }
+    fmt.Println(apiKey.OpenWeatherMapApiKey)
+}
+
+// This function gets the api key from the config file
+func loadApiConfig(filename string) (*apiConfigData, error){
+	// Open the api config file
+    file, err := os.Open(filename)
+    if err != nil {
+        fmt.Println("Error opening config file:", err)
+        return nil, err
+    }
+    defer file.Close()
+
+    // Decode the api config file
+    var config apiConfigData
+    err = json.NewDecoder(file).Decode(&config)
+    if err != nil {
+        fmt.Println("Error decoding config file:", err)
+        return nil, err
+    }
+		return &config, nil
 }
